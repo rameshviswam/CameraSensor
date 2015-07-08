e72373f2a551a50346e3f583c2d638b5432cbfc7
Ramesh Viswanathan <rviswana@qti.qualcomm.com>
2015-07-08 11:43:46 -0700

    cleaned up server thread

--- CameraSensorApplication/app/src/main/java/com/ramesh/camerasensorapplication/TCPServerThread.java
+++ CameraSensorApplication/app/src/main/java/com/ramesh/camerasensorapplication/TCPServerThread.java
@@ -19,55 +19,70 @@ public class TCPServerThread implements Runnable {
     private ServerSocket server;
     private Socket socket;
 
-    //private DataInputStream streamIn;
     private BufferedReader streamIn;
     private DataOutputStream streamOut;
     //private PrintWriter mWriter;
 
     @Override
     public void run() {
-        try
-        {  System.out.println("Binding to port " + SERVER_PORT + ", please wait  ...");
-            server = new ServerSocket(SERVER_PORT);
-            Log.e("RV..............","Server started: " + server);
-            Log.e("RV............", "Waiting for a client ...");
-            socket = server.accept();
-            Log.e("RV............", ("Client accepted: " + socket));
-            open();
+        try {
+            openSocket();
+            openStream();
             boolean done = false;
-            while (!done)
-            {  try {
-                // String line = streamIn.readUTF();
-                String line = streamIn.readLine();
-                Log.e("RV........", line);
-                done = line.equals("bye");
-                String newOut = new String(line);
-                Log.e("RV......", newOut);
-                streamOut.writeBytes(newOut);
-                streamOut.writeBytes("\r\n");
+            while (!done) {
+                try {
+                    // String line = streamIn.readUTF();
+                    String line = streamIn.readLine();
+                    Log.e("RV........", line);
+                    done = line.equals("bye");
+                    String newOut = new String(line);
+                    Log.e("RV......", newOut);
+                    streamOut.writeBytes(newOut);
+                    streamOut.writeBytes("\r\n");
+                } catch (IOException ioe) {
+                    Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
+                    done = true;
+                }
             }
-            catch(IOException ioe)
-            {
-                Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
-                done = true;
-            }
-            }
-            close();
-        }
-        catch(IOException ioe)
-        {
+            closeStream();
+        } catch (IOException ioe) {
             Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
         }
+        finally {
+            try {
+                closeSocket();
+            } catch(IOException ioe) {
+                Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
+            }
+        }
     }
-    public void open() throws IOException
-    {
+
+    private void openSocket() throws IOException {
+        System.out.println("Binding to port " + SERVER_PORT + ", please wait  ...");
+        server = new ServerSocket(SERVER_PORT);
+        Log.e("RV..............", "Server started: " + server);
+
+        Log.e("RV............", "Waiting for a client ...");
+        socket = server.accept();
+        Log.e("RV............", ("Client accepted: " + socket));
+    }
+
+    private void openStream() throws IOException {
         //streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         streamIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         streamOut = new DataOutputStream(socket.getOutputStream());
     }
-    public void close() throws IOException
-    {  if (socket != null)    socket.close();
-        if (streamIn != null)  streamIn.close();
-        if (streamOut != null) streamOut.close();
+
+    private void closeSocket() throws IOException {
+        if (socket != null)
+            socket.close();
+    }
+
+    private void closeStream() throws IOException {
+        if (streamIn != null)
+            streamIn.close();
+
+        if (streamOut != null)
+            streamOut.close();
     }
 }
